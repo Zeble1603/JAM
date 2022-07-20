dc39e79104f6138ebf065c894257e9aec4c1040f
Blaise <blaisedecarvalho@gmail.com>
2022-07-20 16:07:33 +0200

    pull from dev

--- models/User.model.js
+++ models/User.model.js
@@ -8,13 +8,13 @@ const userSchema = new Schema({
   type:{type:String,enum:['host','musician'],required: true},
   //Musicians
   instrument:{type:String,enum:['Guitar','Drums','Bass','Piano','Singer','Harmonica','Saxophone','Trumpet','Other']},
-  instrumentLevel:{type:String,enum:['beginner','intermediate','advanced','expert','I am the fucking Kurt Cobain']},
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
   eventsSubscribed:[{type:Schema.Types.ObjectId, ref:'Jam'}],
   eventsParticipated:[{type:Schema.Types.ObjectId, ref:'Jam'}], //dudas aqui
   eventsNotParticipated:[{type:Schema.Types.ObjectId, ref:'Jam'}],
   participationRating:{type:Number},
   friends:[{ type: Schema.Types.ObjectId, ref: 'User' }],
+  coordinates:{type:Object},
   //Hosts
   jamsCreated:[{type:Schema.Types.ObjectId, ref:'Jam'}],
   jamsPassed:[{type:Schema.Types.ObjectId, ref:'Jam'}],
